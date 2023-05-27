c191bb50d60b37768991d26180c4a6fd5e7ac6ea
Handika <handikaharianto01@gmail.com>
2023-05-27 22:10:48 +0800

    Added charity model

---
+++ lib/data/charity.dart
@@ -0,0 +1,13 @@
+class Charity {
+  final String title;
+  final String description;
+  final String image;
+  final double distance;
+
+  Charity({
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.distance,
+  });
+}
