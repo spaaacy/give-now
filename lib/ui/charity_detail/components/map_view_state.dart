f5f1f7d7a44a6249d9679104c508d5d200676240
Aakif Ahamath <aakifahamath@gmail.com>
2023-05-28 00:55:12 +0800

    Added flutter map and other stuff

---
+++ lib/ui/charity_detail/components/map_view_state.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+class MapViewState extends ChangeNotifier {
+
+  final mapController = MapController();
+
+}
