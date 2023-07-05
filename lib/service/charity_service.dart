e904f0f887d95529ced153f13c0817ea2cfeb66d
Aakif Ahamath <aakifahamath@gmail.com>
2023-07-05 12:27:38 +0800

    Added gifs and fixed charity list

--- lib/service/charity_service.dart
+++ lib/service/charity_service.dart
@@ -9,8 +9,8 @@ final List<dynamic> charityList = [
     description:
     'Standard Chartered is a reputable international bank committed to making a positive and lasting impact on society. Through its charitable initiatives, Standard Chartered strives to create a better future for communities around the world',
     image:
-    'https://scontent-kul2-1.xx.fbcdn.net/v/t39.30808-6/339740568_746157476919433_5396382582645391439_n.png?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=YDAwgM8M3pwAX-194D4&_nc_ht=scontent-kul2-1.xx&oh=00_AfCIlCo1RmOivZ9KAvhI0CbkJiH-O4KZVX666Ee_WDzfow&oe=6477CB96',
-    distance: 0.5,
+    'https://scontent.fkul10-1.fna.fbcdn.net/v/t39.30808-6/339740568_746157476919433_5396382582645391439_n.png?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=OWE3V2eKIzkAX_NBL2x&_nc_ht=scontent.fkul10-1.fna&oh=00_AfCoalNwqCL0P4XU5rB8rVcwJuMdUXcnvqbYKADFoNBX9g&oe=64A93BD6',
+    distance: 1.5,
     donationGoal: 50000,
     currentDonation: 40000,
     latitudeLongitude: const LatLng(
@@ -24,7 +24,7 @@ final List<dynamic> charityList = [
     "The World Health Organization (WHO) is a global organization dedicated to promoting health, preventing diseases, and improving the well-being of all people worldwide.",
     image:
     'https://delinkage.org/wp-content/uploads/2017/04/who-logo.jpg',
-    distance: 0.5,
+    distance: 2.5,
     donationGoal: 50000,
     currentDonation: 39000,
     latitudeLongitude: const LatLng(
@@ -38,7 +38,7 @@ final List<dynamic> charityList = [
     "SOLS 24/7 is a non-profit organization dedicated to transforming lives and communities through education and social empowerment.",
     image:
     'https://upload.wikimedia.org/wikipedia/commons/d/d8/Science_of_Life_Studies_Logo_big.jpg',
-    distance: 1.0,
+    distance: 5.0,
     donationGoal: 35000,
     currentDonation: 36128,
     latitudeLongitude: const LatLng(
@@ -51,8 +51,8 @@ final List<dynamic> charityList = [
     description:
     "Malaysian Care is a charitable organization committed to serving the needs of marginalized and disadvantaged communities in Malaysia",
     image:
-    'https://scontent-hou1-1.xx.fbcdn.net/v/t39.30808-6/348253103_1869960913384280_6408209286534822697_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=vwgA6blyYgUAX9JlFKR&_nc_ht=scontent-hou1-1.xx&oh=00_AfA2Hlv-uFtwX6Hbm2eq7EVFa9Cz68BTfJsbFoITSruETQ&oe=6476E56C',
-    distance: 1.0,
+    'https://scontent.fkul10-1.fna.fbcdn.net/v/t39.30808-6/348253103_1869960913384280_6408209286534822697_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=nMjIS0e2iRkAX_MW4Te&_nc_ht=scontent.fkul10-1.fna&oh=00_AfDHH89QjE4cdEXwLwBVmp0vRf8A0ua3C-Ig0EOkHVR8Qg&oe=64AA4FEC',
+    distance: 5.5,
     donationGoal: 35000,
     currentDonation: 36128,
     latitudeLongitude: const LatLng(
@@ -65,8 +65,8 @@ final List<dynamic> charityList = [
     description:
     "Mercy Mission Malaysia is a charitable organization dedicated to providing relief, empowerment, and sustainable solutions to individuals and communities in need.",
     image:
-    'https://scontent-hou1-1.xx.fbcdn.net/v/t1.18169-9/21768454_1475720132497794_5330420396037508692_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_ohc=05SqKmDklREAX_UETRt&_nc_ht=scontent-hou1-1.xx&oh=00_AfA_1j-ud1F9D9byqch_gXQVVc95gdUhnzeoYgEULeVkow&oe=6499E72D',
-    distance: 1.0,
+    'https://www.mercymission.my/wp-content/uploads/2020/09/mercy-mission-favicon.png',
+    distance: 8.3,
     donationGoal: 35000,
     currentDonation: 36128,
     latitudeLongitude: const LatLng(
